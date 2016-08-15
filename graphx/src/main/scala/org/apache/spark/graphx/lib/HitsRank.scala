d0166be6347ddbd0015162a692fd9b66d16dae9c
Andrew Torson <andrew.torson@adept.com>
2016-08-15 14:47:51 -0700

    Minor revision based on first code review: scala code style fixes etc.

--- graphx/src/main/scala/org/apache/spark/graphx/lib/HitsRank.scala
+++ graphx/src/main/scala/org/apache/spark/graphx/lib/HitsRank.scala
@@ -1,20 +1,18 @@
 /*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *  * Licensed to the Apache Software Foundation (ASF) under one or more
- *  * contributor license agreements.  See the NOTICE file distributed with
- *  * this work for additional information regarding copyright ownership.
- *  * The ASF licenses this file to You under the Apache License, Version 2.0
- *  * (the "License"); you may not use this file except in compliance with
- *  * the License.  You may obtain a copy of the License at
- *  *
- *  *    http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.apache.spark.graphx.lib
@@ -27,85 +25,94 @@ import org.apache.spark.internal.Logging
 import org.apache.spark.rdd.RDD
 
 
-
 /**
  * Implements the HITS (also known as 'hubs and authorities') iterative graph algorithm
  * The algorithm accepts a focused connected graph of linked pages/documents as its input
  * and produces normalized (hub,authority) vertex rank tuple
+ *
  * @see [[http://en.wikipedia.org/wiki/HITS_algorithm]]
  */
 object HitsRank extends Logging {
 
-  // static keys to extra algorithmic output
-  /**
-   * static key to access the actual # of iterations from extra algorithmic output
-   * @usecase runWithExtraInfo()
-   */
-  val AlgIterationsNumber: String = "AlgIterationsNumber"
 
   /**
-   * Executes a given number of HITS algorithm iterations unless convergence tolerance is achieved first.
+   * Executes a given number of HITS algorithm iterations unless
+   * convergence tolerance is achieved first.
    * <P>
    * Each iteration updates HITS (hub,authority) ranks pair for every vertex in a graph
    * The update routine has two phases:
    * <OL>
-   *  <LI> Update vertex Authority rank based on Hub ranks of the vertex incoming neighbors
-   *  <LI> Update vertex Hub rank based on Authority ranks of the vertex outgoing neighbors
+   * <LI> Update vertex Authority rank based on Hub ranks of the vertex incoming neighbors
+   * <LI> Update vertex Hub rank based on Authority ranks of the vertex outgoing neighbors
    * </OL>
    * Each update phase normalizes the computed ranks
    * as is required for the algorithm convergence. L,,2,, norm is used in this implementation:
    * the norm choice affects convergence rate but not the result (up to a scale)
    * <P>
    * The HITS algorithm is highly similar to the Power-Method
-   * used for iterative approximate computation of the dominant eigen-vector(as in Principal Component Analysis).
+   * used for iterative approximate computation of
+   * the dominant eigen-vector(as in Principal Component Analysis).
    * Specifically, the HITS should converge to:
    * <OL>
-   *  <LI> Hub rank: principal unit eigen-vector of the matrix '''AA^T^'''
-   *  <LI> Authority rank: principal unit eigen-vector of the matrix '''A^T^A'''
+   * <LI> Hub rank: principal unit eigen-vector of the matrix '''AA^T^'''
+   * <LI> Authority rank: principal unit eigen-vector of the matrix '''A^T^A'''
    * </OL>
    * where '''A''' is the graph adjacency matrix.
    * <P>
    * Convergence rate of the HITS algorithm may be super-exponential in the worst-case scenarios.
    * It can be accelerated by using the Repeated Squaring technique.
-   * This technique requires caching of the powers of matrix '''A''' which is likely to be memory- and/or
-   * communication-cost prohibitive for large graphs.  This HITS algorithm implementation is lean on caching
-   * (only caches the previous and the current iteration's (Hub, Authority) ranks and only for the duration of the iteration)
+   * This technique requires caching of the powers of matrix '''A'''
+   * which is likely to be memory- and/or
+   * communication-cost prohibitive for large graphs.
+   * This HITS algorithm implementation is lean on caching
+   * (only caches the previous and the current iteration's (Hub, Authority)
+   * ranks and only for the duration of the iteration)
    * The Repeated Squaring technique is not used in this implementation
    * <P>
+   *
    * @see [[http://www.math.cornell.edu/~mec/Winter2009/RalucaRemus/Lecture4/lecture4.html]]
    * @see [[https://en.wikipedia.org/wiki/Exponentiation_by_squaring]]
-   * @param graph the graph for which to compute the HITS ranks
-   * @param initValue initial value of HITS ranks to start iterating from (default is emptyRDD)
-   * @param numIter number of iterations of the main algorithm routine (default is 1)
-   * @param convTolerance convergence tolerance(as measured by approximate Mean-Squared-Error, default is 0.0)
+   * @param graph         the graph for which to compute the HITS ranks
+   * @param initValue     initial value of HITS ranks to start iterating from (default is emptyRDD)
+   * @param numIter       number of iterations of the main algorithm routine (default is 1)
+   * @param convTolerance convergence tolerance(approximate Mean-Squared-Error, default is 0.0)
    * @tparam VD type of the graph vertex attribute object
    * @tparam ED type of the graph edge attribute object
    * @return transformed graph containing (Hub,Authority) HITS rank tuple at each vertex
    */
-  def run[VD: ClassTag, ED: ClassTag](graph: Graph[VD, ED],
-      initValue: RDD[(VertexId, (Double, Double))] = SparkContext.getOrCreate().emptyRDD[(VertexId, (Double, Double))],
-      numIter: Int = 1, convTolerance: Double = 0.0): Graph[(Double, Double), ED] = {
-      runWithExtraReturnInfo(graph, initValue, numIter, convTolerance)._1
+  def run[VD: ClassTag, ED: ClassTag](
+      graph: Graph[VD, ED],
+      initValue: RDD[(VertexId, (Double, Double))] = SparkContext.getOrCreate().emptyRDD,
+      numIter: Int = 1,
+      convTolerance: Double = 0.0): Graph[(Double, Double), ED] = {
+
+    runWithExtraReturnInfo(graph, initValue, numIter, convTolerance)._1
   }
 
   /**
    * Extended output version of the run() method
-   * In addition, returns a value object describing how HITS algorithm performed
+   * In addition, returns a value object describing how HITS algorithm performed.
+   * The value object is a map of [[org.apache.spark.graphx.lib.HitsAlgorithmMetric]]
+   * keyed by the metric case Class(indicates metric uniqueness within the map,
+   * enforced within this run() method)
    *
-   * @param graph the graph for which to compute the HITS ranks
-   * @param initValue initial value of HITS ranks to start iterating from (default is emptyRDD)
-   * @param numIter number of iterations of the main algorithm routine (default is 1)
-   * @param convTolerance convergence tolerance(as measured by approximate Mean-Squared-Error, default is 0.0)
+   * @param graph         the graph for which to compute the HITS ranks
+   * @param initValue     initial value of HITS ranks to start iterating from (default is emptyRDD)
+   * @param numIter       number of iterations of the main algorithm routine (default is 1)
+   * @param convTolerance convergence tolerance(approximate Mean-Squared-Error, default is 0.0)
    * @tparam VD type of the graph vertex attribute object
    * @tparam ED type of the graph edge attribute object
    * @return HITS graph and value object with algorithm performance details
    */
-    def runWithExtraReturnInfo[VD: ClassTag, ED: ClassTag](graph: Graph[VD, ED],
-        initValue: RDD[(VertexId, (Double, Double))] = SparkContext.getOrCreate().emptyRDD[(VertexId, (Double, Double))],
-        numIter: Int = 1, convTolerance: Double = 0.0): (Graph[(Double, Double), ED], Map[String, Any]) = {
+  private [lib] def runWithExtraReturnInfo[VD: ClassTag, ED: ClassTag](
+      graph: Graph[VD, ED],
+      initValue: RDD[(VertexId, (Double, Double))] = SparkContext.getOrCreate().emptyRDD,
+      numIter: Int = 1,
+      convTolerance: Double = 0.0): (Graph[(Double, Double), ED],
+                                     Map[Class[_ <: HitsAlgorithmMetric], HitsAlgorithmMetric]) = {
 
     require(numIter > 0, s"Number of iterations must be greater than 0," +
-      s" but got ${numIter}")
+      s" but got ($numIter)")
 
     // the condition below guarantees that there will never be
     // a situation with zero norm of the ranks collection
@@ -114,15 +121,15 @@ object HitsRank extends Logging {
     val useConvergence: Boolean = convTolerance > 0.0
 
     // Initialize the HITS graph vertex with initial values
-    var hitsGraph: Graph[(Double, Double), ED] = graph
-      .outerJoinVertices[(Double,Double),(Double,Double)](initValue)(
+    var hitsGraph: Graph[(Double, Double), ED] = graph.
+      outerJoinVertices(initValue)(
       (id, oldVal, initHitsVal) => initHitsVal.getOrElse((1.0, 1.0)))
 
     // materialize initial HITS values
     hitsGraph.vertices.foreachPartition(x => {})
 
     var convergence = false
-    var extraAlgOutput = Map[String, Any]()
+    var extraAlgOutput = Map[Class[_ <: HitsAlgorithmMetric], HitsAlgorithmMetric]()
 
     // Perform a fixed number of iterations
     for (i <- 0 until numIter if !convergence) {
@@ -138,7 +145,8 @@ object HitsRank extends Logging {
       // merge the Authority rank into the overall HITS rank graph
       // the missing zero in-degree nodes are set with 0.0 auth rank
       // such nodes do not affect and are not needed for the preceding normalization calculation
-      hitsGraph = hitsGraph.outerJoinVertices[Double, (Double,Double)](normalizeRanksUsingL2Norm(authRanks))(
+      hitsGraph = hitsGraph.outerJoinVertices(
+        normalizeRanksUsingL2Norm(authRanks))(
         (id, oldHitsVal, newAuthVal) => (oldHitsVal._1, newAuthVal.getOrElse(0.0)))
 
 
@@ -151,7 +159,8 @@ object HitsRank extends Logging {
       // merge the Hub rank into the overall HITS rank graph
       // the missing zero out-degree nodes are set with 0.0 hub rank
       // such nodes do not affect and are not needed for the preceding normalization calculation
-      hitsGraph = hitsGraph.outerJoinVertices[Double, (Double,Double)](normalizeRanksUsingL2Norm(hubRanks))(
+      hitsGraph = hitsGraph.outerJoinVertices(
+        normalizeRanksUsingL2Norm(hubRanks))(
         (id, oldHitsVal, newHubVal) => (newHubVal.getOrElse(0.0), oldHitsVal._2))
 
       hitsGraph.vertices.cache()
@@ -159,13 +168,14 @@ object HitsRank extends Logging {
       // materialize the new HITS vertex values: allows to clean up after each iteration
       hitsGraph.vertices.foreachPartition(x => {})
 
-      //lazy MSE error definition
-      lazy val mseProxy = computeHitsMSEProxy(prev.vertices, hitsGraph.vertices)
+      // lazy MSE error definition
+      lazy val mseProxy = computeHitsMSEProxy(
+        prev.vertices, hitsGraph.vertices)
 
       convergence = useConvergence && (mseProxy <= convTolerance)
 
       // log lazily
-      logDebug("Mean-Square-Difference at HITS algorithm iteration #" + (i+1) + " : " + mseProxy)
+      logDebug("Mean-Square-Difference at HITS algorithm iteration #" + (i + 1) + " : " + mseProxy)
 
       // clean up cache
       authRanks.unpersist(false)
@@ -173,38 +183,41 @@ object HitsRank extends Logging {
       prev.unpersist(false)
 
       // just put the algIterationsNumber so far: it is cheap to provide
-      extraAlgOutput += (AlgIterationsNumber -> (i+1))
+      extraAlgOutput += (classOf[HitsIterations] -> HitsIterations(i + 1))
     }
 
     // return final HITS rank with extra alg output
 
-   (hitsGraph, extraAlgOutput)
+    (hitsGraph, extraAlgOutput)
 
   }
 
   /**
-   * Computes the L2 norm of the difference between previous and last HITS iteration rank values
+   * Computes the L2 norm of the difference between
+   * previous and last HITS iteration rank values
    * This is a good convergence indicator, a proxy for MSE error
    * Expensive to compute - so should be used lazily
+   *
    * @param prev previous HITS ranks
    * @param next current HITS ranks
    * @return single floating value of the L2 norm of the difference
    */
-  def computeHitsMSEProxy(prev: VertexRDD[(Double,Double)], next: VertexRDD[(Double,Double)]): Double = {
+  def computeHitsMSEProxy(
+      prev: VertexRDD[(Double, Double)],
+      next: VertexRDD[(Double, Double)]): Double = {
 
-    val errors = prev.innerJoin[(Double, Double), (Double, Double)](next)((vid, x, y) => (x._1 - y._1, x._2 - y._2))
+    val errors: VertexRDD[(Double, Double)] = prev.innerJoin(next)(
+      (vid, x, y) => (x._1 - y._1, x._2 - y._2))
 
-    val error = errors.map[(Double, Double)](x => (x._2._1 * x._2._1, x._2._2 * x._2._2)).reduce(
+    val er: (Double, Double) = errors.map(x => (x._2._1 * x._2._1, x._2._2 * x._2._2)).reduce(
       (x, y) => (x._1 + y._1, x._2 + y._2))
 
-    error match {
-      case (x: Double, y: Double) => math.sqrt(x + y)
-      case _ => 0.0
-    }
+    math.sqrt(er._1 + er._2)
   }
 
   /**
    * Normalize ranks using L2 norm
+   *
    * @param ranks rdd of vertex ranks
    * @return L2 normalized vertex ranks
    */
@@ -214,11 +227,27 @@ object HitsRank extends Logging {
     // this operation is expensive but is required for convergence
     // it materializes input ranks (should be previously marked for caching by the caller)
     // the norm below is computed only once and is enclosed by the returned RDD
-    val norm: Double = math.sqrt(ranks.map[Double]( x => (x._2 * x._2)).reduce(_+_))
+    val norm: Double = math.sqrt(ranks.map(x => x._2 * x._2).reduce(_ + _))
 
-    if (norm > 0.0) ranks.mapValues( a => a / norm ) else ranks
+    if (norm > 0.0) ranks.mapValues(a => a / norm) else ranks
 
   }
 
 }
+
+
+/**
+ * Defines a case-class family of HITS algorithm performance metrics
+ *
+ * @usecase runWithExtraInfo()
+ */
+abstract class HitsAlgorithmMetric
+
+
+/**
+ * Actual number of HITS algorithm iterations taken
+ * @param iterNum number of iterations
+ */
+case class HitsIterations(iterNum: Int) extends HitsAlgorithmMetric
+
 
--- graphx/src/test/scala/org/apache/spark/graphx/lib/HitsRankSuite.scala
+++ graphx/src/test/scala/org/apache/spark/graphx/lib/HitsRankSuite.scala
@@ -1,58 +1,73 @@
 /*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *  * Licensed to the Apache Software Foundation (ASF) under one or more
- *  * contributor license agreements.  See the NOTICE file distributed with
- *  * this work for additional information regarding copyright ownership.
- *  * The ASF licenses this file to You under the Apache License, Version 2.0
- *  * (the "License"); you may not use this file except in compliance with
- *  * the License.  You may obtain a copy of the License at
- *  *
- *  *    http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.apache.spark.graphx.lib
 
 import scala.reflect.ClassTag
+
 import org.scalactic.{Equality, TolerantNumerics}
 
 import org.apache.spark.SparkFunSuite
 import org.apache.spark.graphx._
 
+
+
 class HitsRankSuite extends SparkFunSuite with LocalSparkContext {
 
-  type Hits = (VertexId, (Double,Double))
+  type Hits = (VertexId, (Double, Double))
 
   /**
-   * Utility function for scaling of HITS rank to match a pair of expected HITS ranks.
-   * Makes it easier to compare scaled ranks with non-canonically scaled expectations
-   * @param expectedHub expected HITS rank to scale Hub ranks to
+   * Utility function for scaling of HITS rank
+   * to match a pair of expected HITS ranks.
+   * Makes it easier to compare scaled ranks
+   * with non-canonically scaled expectations
+   *
+   * @param expectedHub  expected HITS rank to scale Hub ranks to
    * @param expectedAuth expected HITS ranks to scale Authority ranks to
-   * @param ranks set of HITS ranks to scale
+   * @param ranks        set of HITS ranks to scale
    * @return scaled set of HITS ranks
    */
-  def scaleRankToMatchExpectedElements(expectedHub: Hits, expectedAuth: Hits, ranks: Iterable[Hits]): Iterable[Hits] = {
+  def scaleRankToMatchExpectedElements(
+      expectedHub: Hits,
+      expectedAuth: Hits,
+      ranks: Iterable[Hits]): Iterable[Hits] = {
 
-    assert(expectedHub._2._1 > 0.0, s"Expected Hub rank in ($expectedHub) should be positive for well-defined rank scaling")
-    assert(expectedAuth._2._2 > 0.0, s"Expected Authority rank in ($expectedAuth) should be positive for well-defined rank scaling")
+    assert(expectedHub._2._1 > 0.0,
+      s"Expected Hub rank in ($expectedHub) should be positive for well-defined rank scaling")
+    assert(expectedAuth._2._2 > 0.0,
+      s"Expected Authority rank in ($expectedAuth) should be positive" +
+        s" for well-defined rank scaling")
 
-    val actualHubs  = ranks.filter( x => (x._1 == expectedHub._1))
-    val actualAuths = ranks.filter( x => (x._1 == expectedAuth._1))
+    val actualHubs = ranks.filter(x => x._1 == expectedHub._1)
+    val actualAuths = ranks.filter(x => x._1 == expectedAuth._1)
 
-    assert(actualHubs.size == 1, s"Expected a single vertex in ($actualHubs) with ID matching expected Hub($expectedHub)")
-    assert(actualAuths.size == 1, s"Expected a single vertex in ($actualAuths) with ID matching expected Authority($expectedAuth)")
+    assert(actualHubs.size == 1,
+      s"Expected a single vertex in ($actualHubs) with ID matching expected Hub($expectedHub)")
+    assert(actualAuths.size == 1,
+      s"Expected a single vertex in ($actualAuths) with ID matching" +
+        s" expected Authority($expectedAuth)")
 
-    val actualHub: Hits = actualHubs.toSeq(0)
-    val actualAuth: Hits = actualAuths.toSeq(0)
+    val actualHub: Hits = actualHubs.head
+    val actualAuth: Hits = actualAuths.head
 
-    assert(actualHub._2._1 > 0.0, s"Actual Hub rank in ($actualHub) should be positive for well-defined rank scaling")
-    assert(actualAuth._2._2 > 0.0, s"Actual Authority rank in ($actualAuth) should be positive for well-defined rank scaling")
+    assert(actualHub._2._1 > 0.0,
+      s"Actual Hub rank in ($actualHub) should be positive for well-defined rank scaling")
+    assert(actualAuth._2._2 > 0.0,
+      s"Actual Authority rank in ($actualAuth) should be positive for well-defined rank scaling")
 
     val hubScale = expectedHub._2._1 / actualHub._2._1
     val authScale = expectedAuth._2._2 / actualAuth._2._2
@@ -62,71 +77,89 @@ class HitsRankSuite extends SparkFunSuite with LocalSparkContext {
 
   /**
    * Tolerant Equality comparator of HITS ranks
+   *
    * @param tolerance double-precision tolerance
    * @return true/false equality comparison
    */
   def tolerantHitsRankEquality(tolerance: Double): Equality[Hits] = {
     new Equality[Hits] {
       implicit val deq: Equality[Double] = TolerantNumerics.tolerantDoubleEquality(tolerance)
+
       def areEqual(a: Hits, b: Any): Boolean = {
         b match {
-          case ((vid, (hub,auth))) => (a._1 == vid && a._2._1 === hub && a._2._2 === auth)
+          case ((vid, (hub, auth))) => a._1 == vid && a._2._1 === hub && a._2._2 === auth
           case _ => false
         }
       }
+
       override def toString: String = s"tolerantHitsRankEquality($tolerance)"
     }
   }
 
   /**
    * Tolerant Equality comparator of sets of HITS ranks
+   *
    * @param tolerance double-precision tolerance
    * @return true/false equality comparison
    */
   def tolerantHitsRankCollectionEquality(tolerance: Double): Equality[Array[Hits]] = {
-    new Equality[Array[Hits]]  {
-      implicit val heq: Equality[Hits]  = tolerantHitsRankEquality(tolerance)
+    new Equality[Array[Hits]] {
+      implicit val heq: Equality[Hits] = tolerantHitsRankEquality(tolerance)
+
       def areEqual(a: Array[Hits], b: Any): Boolean = {
         b match {
-          case s: Array[Hits] => {a.size == s.size && a.zip[Hits, Hits, Array[(Hits, Hits)]](s).filterNot(
-            a => heq.areEqual(a._1,a._2) ).isEmpty}
+          case s: Array[Hits] => a.length == s.length &&
+            a.zip[Hits, Hits, Array[(Hits, Hits)]](s).forall(a => heq.areEqual(a._1, a._2))
           case _ => false
         }
       }
+
       override def toString: String = s"tolerantHitsRankCollectionEquality($tolerance)"
     }
   }
 
   /**
    * Runs the HITS test and returns the result after performing basic asserts
-   * @param graph input graph
+   *
+   * @param graph         input graph
    * @param expectedRanks expected HITS ranks
-   * @param numIter number of iterations
-   * @param tolerance numeric double precision tolerance
-   * @param algAsserts extra asserts on algorithm performance
+   * @param numIter       number of iterations
+   * @param tolerance     numeric double precision tolerance
+   * @param algAssert    function implementing extra (independent) asserts on algorithm performance
    * @tparam VD vertex property type
    * @tparam ED edge property type
    * @return HITS results
    */
-  def runHitsTestAndPerformBasicAsserts[VD: ClassTag, ED: ClassTag](graph: Graph[VD, ED],
-      expectedRanks: Seq[Hits], numIter: Int, tolerance: Double,
-      algAsserts: Map[String, Any => Boolean] = Map[String, Any => Boolean]()): Seq[Hits] = {
+  def runHitsTestAndPerformBasicAsserts[VD: ClassTag, ED: ClassTag](
+      graph: Graph[VD, ED],
+      expectedRanks: Seq[Hits],
+      numIter: Int,
+      tolerance: Double,
+      algAssert: HitsAlgorithmMetric => Boolean = _ => true): Seq[Hits] = {
 
-    implicit val hitsEq: Equality[Array[Hits]] = tolerantHitsRankCollectionEquality(math.max(0.0000001, tolerance))
+    implicit val hitsEq: Equality[Array[Hits]] = tolerantHitsRankCollectionEquality(
+      math.max(0.0000001, tolerance))
 
-    val expectedHub: Hits = expectedRanks.find( x => (x._2._1 > 0.0)).getOrElse[Hits]((0L, (0.0,0.0)))
-    val expectedAuth: Hits = expectedRanks.find( x => (x._2._2 > 0.0)).getOrElse[Hits]((0L, (0.0,0.0)))
+    val expectedHub: Hits = expectedRanks.find(
+      x => x._2._1 > 0.0).getOrElse[Hits]((0L, (0.0, 0.0)))
+    val expectedAuth: Hits = expectedRanks.find(
+      x => x._2._2 > 0.0).getOrElse[Hits]((0L, (0.0, 0.0)))
 
-    val resultsExtra = HitsRank.runWithExtraReturnInfo(graph, numIter = numIter, convTolerance = 0.99*tolerance)
+    val resultsExtra = HitsRank.runWithExtraReturnInfo(
+      graph,
+      numIter = numIter,
+      convTolerance = 0.99 * tolerance)
+
     val results = resultsExtra._1.vertices.collect()
-    val extra: Map[String, Any] = resultsExtra._2
+    val extra = resultsExtra._2
 
     val scaledResults = scaleRankToMatchExpectedElements(expectedHub, expectedAuth, results).toSeq
 
     assert(expectedRanks.toArray[Hits].sorted[Hits] === scaledResults.toArray[Hits].sorted[Hits])
 
-    for ((key,value) <- extra){
-      if (algAsserts.contains(key)) assert(algAsserts(key).apply(value), s"Failed test assert keyed by ($key)")
+    for ((key, value) <- extra) {
+       assert(algAssert(value),
+        s"Failed test assert keyed by ($key)")
     }
 
     results.toSeq
@@ -184,7 +217,7 @@ class HitsRankSuite extends SparkFunSuite with LocalSparkContext {
           Seq((1L, 2L), (1L, 3L))), 1)
       // expect equal Authority ranks for Authority nodes
       val expectedRanks: Seq[Hits] = Seq(
-        (1L, (1.0, 0.0)), (2L, (0.0, 1.0/2.0)), (3L, (0.0, 1.0/2.0)))
+        (1L, (1.0, 0.0)), (2L, (0.0, 1.0 / 2.0)), (3L, (0.0, 1.0 / 2.0)))
 
       runHitsTestAndPerformBasicAsserts(graph, expectedRanks, 1, 0.0)
 
@@ -200,13 +233,12 @@ class HitsRankSuite extends SparkFunSuite with LocalSparkContext {
           Seq((1L, 3L), (2L, 3L))), 1)
       // expect equal Hub ranks for Hub nodes
       val expectedRanks: Seq[Hits] = Seq(
-        (1L, (1.0/2.0, 0.0)), (2L, (1.0/2.0, 0.0)), (3L, (0.0, 1.0)))
+        (1L, (1.0 / 2.0, 0.0)), (2L, (1.0 / 2.0, 0.0)), (3L, (0.0, 1.0)))
 
       runHitsTestAndPerformBasicAsserts(graph, expectedRanks, 1, 0.0)
 
     }
   }
-
 
   test("HITS Rank: 2 disconnected asymmetric sub-graphs") {
 
@@ -220,20 +252,18 @@ class HitsRankSuite extends SparkFunSuite with LocalSparkContext {
       // the large hub kills the small: this is the dominant principal-component property of HITS
       val expectedRanks: Seq[Hits] = Seq(
         (1L, (0.0, 0.0)), (2L, (0.0, 0.0)),
-        (3L, (1.0, 0.0)), (4L, (0.0, 1.0/2.0)), (5L, (0.0, 1.0/2.0)))
+        (3L, (1.0, 0.0)), (4L, (0.0, 1.0 / 2.0)), (5L, (0.0, 1.0 / 2.0)))
 
-      def iterCheck: Any => Boolean = {
-        x => x match {
-          case v: Int => v < 10
-          case _ => false
-        }
+      def iterCheck: HitsAlgorithmMetric => Boolean = {
+          case HitsIterations(v) => v < 10
+          case _ => true
       }
 
       // will assert that actually fewer iterations were performed to achieve convergence
-      runHitsTestAndPerformBasicAsserts(graph, expectedRanks, 10, 0.01,
-        Map[String, Any => Boolean](HitsRank.AlgIterationsNumber -> iterCheck))
+      runHitsTestAndPerformBasicAsserts(graph, expectedRanks, 10, 0.01, iterCheck)
 
     }
   }
 
 }
+
